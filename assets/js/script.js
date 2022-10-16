d49d776a1174625b165083e03ce9e5e9c3204fc9
Lisette33 <113862182+Lisette33@users.noreply.github.com>
2022-10-16 18:49:14 -0400

    Adding changes to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,20 @@ var quizBox = document.getElementById('code-quiz');
 var answersbox = document.getElementById('sub-answers');
 var answerButton = document.getElementById('answers');
 
-generatequiz(questions, quizBox, answersbox, answerButton);
-
-
-
-function generatequiz(questions, quizBox, answersbox, answerButton){
 
 //Prompt to start quiz
+answersbox.addEventListener("click",function(){
+    generatequiz(questions, quizBox, answersbox, answerButton);
+    revealQuestions(questions, quizBox);
+    // revealAnswers(questions, quizBox, answersbox);
+})
 
 
 
 //These are the questions.
-var Questions = [
+var questions = [
     {
-        question1: "Commonly used data types Do Not include:",
+        question: "Commonly used data types Do Not include:",
         answers: {
             a: 'strings',
             b: 'booleans',
@@ -26,7 +26,7 @@ var Questions = [
     },
 
     {
-    question2: "The condition in an if / else statement is enclosed with _______.",
+    question: "The condition in an if / else statement is enclosed with _______.",
         answers: {
             a: 'quotes',
             b: 'curly brackets',
@@ -37,7 +37,7 @@ var Questions = [
     },
 
     {
-        question3: "Arrays in JavaScript can be used to store _______.",
+        question: "Arrays in JavaScript can be used to store _______.",
             answers: {
                 a: 'numbers and strings',
                 b: 'other arrays',
@@ -48,7 +48,7 @@ var Questions = [
         },
 
     {
-        question3: "String values must be enclosed within _______ when being assigned to variables.",
+        question: "String values must be enclosed within _______ when being assigned to variables.",
             answers: {
                 a: 'commas',
                 b: 'curly brackets',
@@ -59,7 +59,7 @@ var Questions = [
         },
         
     {
-        question4: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
             answers: {
                 a: 'JavaScript',
                 b: 'terminal/bash',
@@ -70,38 +70,43 @@ var Questions = [
         },
 
 ];
+function generatequiz(questions, quizBox, answersbox, answerButton){
+    alert("Hello")
 }
 
-//Show questions
-revealQuestions(questions, quizBox);
 
+
+//Show questions
 function revealQuestions(questions, quizBox){
     var output = [];
     var answers;
 
-    for(var i=0; i<questions.lenth; i++){
-
+    for(var i=0; i<questions.length; i++){
+        
         answers = [];
 
         for(letter in questions[i].answers){
+            console.log(letter)
 
             answers.push(
             '<label>'
                 + '<input type="radio" name="question'+i+'"value="'+letter+'">'
                 + letter + ': '
                 +questions[i].answers[letter]
-            +'</label' 
+            +'</label>' 
             );
         }
 
         output.push(
-            'div class"question">' + questions[i].questions + '</div>'
+            '<div class="question">' + questions[i].question + '</div>'
             + '<div class="answers">' + answers.join('') + '</div>'
+            + '<button class="nextButton">next</button>'
         );
     }
 
     quizBox.innerHTML = output.join('');
 }
+
 
 //If user presses submit, reveal results
 answerButton.onclick = function(){
@@ -115,7 +120,8 @@ answerButton.onclick = function(){
 
 }
 
-//Reveal the answers after user presses submit
+
+//Reveal the answers after user pressed submit
 function revealAnswers(questions, quizBox, answersbox){
    
     var answersbox = quizBox.querySelectorAll('.answers');
@@ -140,6 +146,3 @@ function revealAnswers(questions, quizBox, answersbox){
    answersbox.innerHTML = amountcorrectAnswers + ' out of ' + questions.length; i++;
 }
    
-answerButton.onclick = function(){
-    revealAnswers(questions, quizBox, answersbox);
-}
